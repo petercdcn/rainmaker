4e6ac8d5bcfded3614f231cd6e2e2b37ac22e0d0
Shifan Wu <shifan.wu@concur.com>
2019-01-06 16:00:47 -0800

    Signup, login, and home page.  No backend

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+        <h1>RainMaker</h1>
+        <p>Make savings for Auto Insurance Easy</p>
+        </div>
+      </div>
+    )
+  }
+}
